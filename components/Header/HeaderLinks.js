de6d29c610f337b78375b45422f342f17a43e4a4
jason <jason@portable.com.au>
2020-05-12 22:21:22 +1000

    Fix errors

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
@@ -18,69 +18,68 @@ import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
 
 import styles from "assets/jss/nextjs-material-kit/components/headerLinksStyle.js";
 import Button from "../../components/CustomButtons/Button";
+import Link from "next/link";
 
 const useStyles = makeStyles(styles);
 
 export default function HeaderLinks(props) {
-  const classes = useStyles();
-  return (
-    <List className={classes.list}>
+    const classes = useStyles();
+    return (
+        <List className={classes.list}>
 
-        <ListItem className={classes.listItem}>
-            <Button
-                href={"/news"}
-                color="transparent"
-                className={classes.navLink}
-            >
-                <AnnouncementIcon className={classes.icons} /> News
-            </Button>
-        </ListItem>
-        <ListItem className={classes.listItem}>
-            <Button
-                href={"/races"}
-                color="transparent"
-                className={classes.navLink}
-            >
-                <EventIcon className={classes.icons} /> Calendar
-            </Button>
-        </ListItem>
-        {/*<ListItem className={classes.listItem}>*/}
-        {/*    <Button*/}
-        {/*        href={"/races"}*/}
-        {/*        color="transparent"*/}
-        {/*        className={classes.navLink}*/}
-        {/*    >*/}
-        {/*        <DirectionsBikeIcon className={classes.icons} /> Results*/}
-        {/*    </Button>*/}
-        {/*</ListItem>*/}
-        {/*<ListItem className={classes.listItem}>*/}
-        {/*    <Button*/}
-        {/*        href={"/races"}*/}
-        {/*        color="transparent"*/}
-        {/*        target="_blank"*/}
-        {/*        className={classes.navLink}*/}
-        {/*    >*/}
-        {/*        <MapIcon className={classes.icons} /> Circuits*/}
-        {/*    </Button>*/}
-        {/*</ListItem>*/}
+            <ListItem className={classes.listItem}>
+                <Link
+                    href={"/news"}
 
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-facebook"
-          title="Find us on facebook"
-          placement={"top"}
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href={"https://www.facebook.com/EurekaVeteransCyclingClub/"}
-            target="_blank"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-facebook"} />
-          </Button>
-        </Tooltip>
-      </ListItem>
-    </List>
-  );
+                >
+                    <a className={classes.navLink}><AnnouncementIcon className={classes.icons}/> News</a>
+                </Link>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Link
+                    href={"/races"}
+                >
+                    <a className={classes.navLink}
+                    ><EventIcon className={classes.icons}/> Calendar</a>
+                </Link>
+            </ListItem>
+            {/*<ListItem className={classes.listItem}>*/}
+            {/*    <Button*/}
+            {/*        href={"/races"}*/}
+            {/*        color="transparent"*/}
+            {/*        className={classes.navLink}*/}
+            {/*    >*/}
+            {/*        <DirectionsBikeIcon className={classes.icons} /> Results*/}
+            {/*    </Button>*/}
+            {/*</ListItem>*/}
+            {/*<ListItem className={classes.listItem}>*/}
+            {/*    <Button*/}
+            {/*        href={"/races"}*/}
+            {/*        color="transparent"*/}
+            {/*        target="_blank"*/}
+            {/*        className={classes.navLink}*/}
+            {/*    >*/}
+            {/*        <MapIcon className={classes.icons} /> Circuits*/}
+            {/*    </Button>*/}
+            {/*</ListItem>*/}
+
+            <ListItem className={classes.listItem}>
+                <Tooltip
+                    id="instagram-facebook"
+                    title="Find us on facebook"
+                    placement={"top"}
+                    classes={{tooltip: classes.tooltip}}
+                >
+                    <a
+                        color="transparent"
+                        href={"https://www.facebook.com/EurekaVeteransCyclingClub/"}
+                        target="_blank"
+                        className={classes.navLink}
+                    >
+                        <i className={classes.socialIcons + " fab fa-facebook"}/>
+                    </a>
+                </Tooltip>
+            </ListItem>
+        </List>
+    );
 }
--- pages/races/index.tsx
+++ pages/races/index.tsx
@@ -13,39 +13,7 @@ import PublicLayout from "../../layouts/public";
 const cvc = require("../../assets/img/vcv.svg")
 
 const useStyles = makeStyles((theme) => createStyles({
-    container: {
-        ...container,
-        zIndex: 12,
-        color: "#FFFFFF"
-    },
-    title: {
-        ...title,
-        display: "inline-block",
-        // position: "relative",
-        marginTop: 30,
-        minHeight: 32,
-        color: "#FFFFFF",
-        textDecoration: "none",
-    },
-    subtitle: {
-        fontSize: "1.313rem",
-        maxWidth: "500px",
-        margin: "10px auto 0"
-    },
-    main: {
-        background: "#FFFFFF",
-        position: "relative",
-        zIndex: 3
-    },
-    mainRaised: {
-        margin: "-60px 30px 0px",
-        borderRadius: "6px",
-        boxShadow:
-            "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
-    },
-    section: {
-        padding: "70px 0",
-    },
+
     description: {
         color: "#999"
     },
