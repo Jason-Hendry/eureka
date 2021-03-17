8daaa8821261159f4747cfc67bfa507db3292b93
jason <jason@portable.com.au>
2021-03-17 22:56:14 +1100

    Fix styling from initial load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,42 @@
 import {AppProps} from "next/app";
 
-import '../assets/scss/nextjs-material-kit.scss'
+// import '../assets/scss/nextjs-material-kit.scss'
 import AdminTheme from "../layout/Admin/AdminTheme";
 import Head from "next/head";
 import {GTMPageView} from "../services/gtm";
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {ThemeProvider} from "@material-ui/styles";
+import {useEffect} from "react";
+import {createMuiTheme} from "@material-ui/core";
+
+const theme = createMuiTheme({
+
+})
 
 function MyApp({ Component, pageProps, router }: AppProps) {
     const admin = router.pathname.match('^/admin');
     if(admin) {
        return <AdminTheme><Component {...pageProps} /></AdminTheme>
     }
+
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles?.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
+
     return <>
         <Head>
             <title>Eureka Cycling</title>
         </Head>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
     </>
 }
 
